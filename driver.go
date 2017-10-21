20e219e5578be880a671f8c6a29f34ba0aaa8a21
Björn Johanson <bjorjoh.dev@gmail.com>
2017-10-21 10:49:22 +0200

    added prime calc

--- driver.go
+++ driver.go
@@ -1,21 +1,35 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"strings"
+)
 
 func main() {
-	var i string = ""
+	var i string
 	var err error
 
 	cont := true
-
 	for cont {
+		fmt.Print("Please enter a number\n")
 		_, err = fmt.Scanf("%s", &i)
 		if err == nil {
 			switch i {
 			case "1":
-				fmt.Println("one")
+				one()
 			case "2":
-				fmt.Println("two")
+				fmt.Printf("What is your name?\n")
+				var name string
+				_, err = fmt.Scanf("%s", &name)
+				two(name)
+			case "3":
+				three()
+			case "4":
+				four()
+			case "5":
+				primeTime()
+			case "6":
+				primeTime()
 			default:
 				fmt.Println("Not an option")
 				cont = false
@@ -24,4 +38,97 @@ func main() {
 			fmt.Printf("Error occurred: %s\n", err)
 		}
 	}
+}
+
+func one() {
+	fmt.Printf("Hello world!\n")
+}
+
+func two(name string) {
+	if strings.EqualFold(name, "Alice") || strings.EqualFold(name, "Bob") {
+		fmt.Printf("Hello %s\n", name)
+	} else {
+		one()
+	}
+
+}
+
+func three() {
+	//var num int
+	fmt.Println("Please enter a number")
+
+	var i int
+	_, e := fmt.Scanf("%d", &i)
+	if e != nil {
+		err(e)
+	} else {
+		fmt.Printf("The sum is: %d\n", sumCalc(i))
+	}
+}
+
+func err(e error) {
+	fmt.Println("An error has occured")
+	fmt.Println(e)
+}
+
+func sumCalc(num int) int {
+	sum := 0
+	for i := num; i > 0; i-- {
+		sum += i
+	}
+	return sum
+}
+
+func sumCalcDiv36(num int) int {
+	sum := 0
+	for i := num; i > 0; i-- {
+		if i%3 == 0 || i%5 == 0 {
+			sum += i
+		}
+	}
+	return sum
+}
+
+func getDigitFromUser() int {
+	fmt.Println("Please enter a digit")
+	i := 0
+	_, err := fmt.Scanf("%d", &i)
+	if err != nil {
+		fmt.Println("Error while getting int from user")
+		fmt.Println(err)
+	}
+	return i
+}
+
+func four() {
+	input := getDigitFromUser()
+	printMultTable(input)
+}
+
+func printMultTable(num int) {
+	for i := 0; i < 12; i++ {
+		fmt.Printf("%d * %d = %d\n", i, num, num*i)
+	}
+}
+
+func primeTime() {
+	for i := 2; i < 100; i++ {
+		if isPrime(i) {
+			fmt.Println(i)
+		}
+	}
+}
+
+func isPrime(num int) bool {
+	isaPrime := true
+	if num < 2 {
+		isaPrime = false
+		return isaPrime
+	}
+	for i := 2; i < num; i++ {
+		if num%i == 0 {
+			isaPrime = false
+		}
+	}
+	return isaPrime
 }
--- driver_test.go
+++ driver_test.go
@@ -1,23 +1,37 @@
 package main
 
 import (
-	"os/exec"
-	"strings"
 	"testing"
 )
 
-func TestCorrectInput(t *testing.T) {
-
-	subproc := exec.Command("go_playground")
-	input := "1\n"
-	reader := strings.NewReader(input)
-	subproc.Stdin = reader
-	output, _ := subproc.Output()
-
-	s := string(output)
-	if "one\n" != s {
-		t.Errorf("Wanted: %s, Got: %s", "one", s)
+func TestSumCalc(t *testing.T) {
+	if sumCalc(5) != 15 {
+		t.Error("failed")
+	} else if sumCalc(3) != 6 {
+		t.Error("failed")
 	}
 
-	subproc.Process.Kill()
+}
+
+func TestSumCalc60(t *testing.T) {
+	if sumCalcDiv36(5) != 8 {
+		t.Error("Failed")
+	} else if sumCalcDiv36(15) != 60 {
+		t.Errorf("Failed! expected %d, actual %d", 60, sumCalcDiv36(15))
+	}
+
+}
+
+func TestIsPrime(t *testing.T) {
+	if !isPrime(19) {
+		t.Error("Failed, 19 is a prime")
+	} else if !isPrime(15485863) {
+		t.Error("22,338,618 is a prime")
+	} else if isPrime(1) {
+		t.Error("Failed 1 is not a prime")
+	} else if isPrime(-1) {
+		t.Error("Primes can not be negative")
+	} else if isPrime(0) {
+		t.Error("0 is not a prime")
+	}
 }
